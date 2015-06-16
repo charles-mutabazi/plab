4035625d59d6ee9d69f0faa733708f3a30365da4
Mutabazi C <charl.mutabazi@gmail.com>
2015-06-16 21:22:25 +0900

    Add CRUD functions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,26 @@ class PostsController < ApplicationController
 		redirect_to @post
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to root_path
+	end
+
 	private
 	def post_params
 		params.require(:post).permit(:title, :body)
