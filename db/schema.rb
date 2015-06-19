da529c8dca001e2cd6879c7b000d1f1fcae4499f
Mutabazi C <charl.mutabazi@gmail.com>
2015-06-19 23:56:49 +0900

    Added biography and user's full name fields, styled members page as well

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150618094257) do
+ActiveRecord::Schema.define(version: 20150619124458) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20150618094257) do
     t.datetime "updated_at",                          null: false
     t.string   "username"
     t.string   "full_name"
+    t.text     "biography"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
