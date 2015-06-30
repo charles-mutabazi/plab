eff8e55b8036d5200f71f4f594b56087cf3eb7c3
Mutabazi C <charl.mutabazi@gmail.com>
2015-06-30 11:18:41 +0900

    Added user image field in the database and added some

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150619124458) do
+ActiveRecord::Schema.define(version: 20150629093214) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20150619124458) do
     t.string   "username"
     t.string   "full_name"
     t.text     "biography"
+    t.string   "image_path"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
